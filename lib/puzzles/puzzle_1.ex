ffe9635b2cbdf71e899e458c322e40aecfa0c782
PKriwin <paulkriw@gmail.com>
2020-12-04 21:49:11 +0100

    ⭐️ first puzzle implementation

--- lib/puzzles/puzzle_1.ex
+++ lib/puzzles/puzzle_1.ex
@@ -2,8 +2,18 @@ defmodule AdventOfCode.Puzzle1 do
   alias AdventOfCode.Utils.{InputReader}
 
   def resolve do
-    input = InputReader.read_input(1)
+    InputReader.read_input(1)
+    |> Enum.map(&String.to_integer/1)
+    |> find_result
+    |> IO.puts()
+  end
 
-    IO.puts(input)
+  def find_result([_ | []]), do: "no result"
+
+  def find_result([number | tail]) do
+    case tail |> Enum.find(fn item -> number + item == 2020 end) do
+      nil -> find_result(tail)
+      value -> number * value
+    end
   end
 end
