a2a7669705d83694ba617de59b23363f0f320aa8
PKriwin <paulkriw@gmail.com>
2021-03-25 08:49:46 +0000

    ⭐️ add implementation of part 2 of puzzle 22

--- lib/puzzles/puzzle_22.ex
+++ lib/puzzles/puzzle_22.ex
@@ -15,25 +15,53 @@ defmodule AdventOfCode.Puzzle22 do
     |> Enum.reduce(0, fn {points, coef}, score -> score + points * coef end)
   end
 
-  def play_combat(deck1, deck2) do
-    case [deck1, deck2] do
-      [[card1 | deck1], [card2 | deck2]] ->
+  def play_combat(decks) do
+    case decks do
+      {[card1 | deck1], [card2 | deck2]} ->
         cond do
-          card1 > card2 -> play_combat(deck1 ++ [card1, card2], deck2)
-          card1 < card2 -> play_combat(deck1, deck2 ++ [card2, card1])
+          card1 > card2 -> play_combat({deck1 ++ [card1, card2], deck2})
+          card1 < card2 -> play_combat({deck1, deck2 ++ [card2, card1]})
         end
 
-      decks ->
-        decks |> Enum.find(&(length(&1) != 0)) |> score()
+      {deck1, []} ->
+        {:player1, deck1}
+
+      {[], deck2} ->
+        {:player2, deck2}
     end
   end
 
-  def play_recursive_combat(deck1, deck2) do
+  def play_recursive_combat(decks, mem \\ %MapSet{})
+  def play_recursive_combat({deck1, []}, _mem), do: {:player1, deck1}
+  def play_recursive_combat({[], deck2}, _mem), do: {:player2, deck2}
+
+  def play_recursive_combat(decks, mem) do
+    if MapSet.member?(mem, decks) do
+      {:player1, elem(decks, 0)}
+    else
+      {[card1 | deck1], [card2 | deck2]} = decks
+
+      if length(deck1) >= card1 and length(deck2) >= card2 do
+        play_recursive_combat({Enum.slice(deck1, 0, card1), Enum.slice(deck2, 0, card2)})
+        |> case do
+          {:player1, _} ->
+            play_recursive_combat({deck1 ++ [card1, card2], deck2}, MapSet.put(mem, decks))
+
+          {:player2, _} ->
+            play_recursive_combat({deck1, deck2 ++ [card2, card1]}, MapSet.put(mem, decks))
+        end
+      else
+        cond do
+          card1 > card2 ->
+            play_recursive_combat({deck1 ++ [card1, card2], deck2}, MapSet.put(mem, decks))
+
+          card1 < card2 ->
+            play_recursive_combat({deck1, deck2 ++ [card2, card1]}, MapSet.put(mem, decks))
+        end
+      end
+    end
   end
 
-  def resolve_first_part, do: parse_input() |> (&play_combat(elem(&1, 0), elem(&1, 1))).()
-
-  def resolve_second_part do
-    parse_input() |> (&play_recursive_combat(elem(&1, 0), elem(&1, 1))).()
-  end
+  def resolve_first_part, do: parse_input() |> play_combat |> (&score(elem(&1, 1))).()
+  def resolve_second_part, do: parse_input() |> play_recursive_combat |> (&score(elem(&1, 1))).()
 end
